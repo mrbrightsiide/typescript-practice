ddcc8dae2706ce1c0eae441ce208d2161e4ce00c
sia kim <devkimsia@gmail.com>
2023-09-26 22:48:07 +0900

    인터페이스와 클래스

--- section6/src/chapter3.ts
+++ section6/src/chapter3.ts
@@ -1,35 +1,23 @@
-//접근 제어자 access modifier
+/*
+인터페이스로 설계도를 먼저 그리고 클래스 구현이 가능하다
+(보통 이렇게 잘 쓰지는 않는데, 정교한 프로그래밍을 해야할 때나 사용하거나 라이브러리 개발/코드 볼 때 유용)
+ */
 
-class Person {
-  public name: string; //기본 접근제어자는 public
-  private age: number; //privaite -> 클래스 외부에서 접근 자체가 불가능함. 메소드 안에서 접근 가능, 파생 클래스에서 접근 불가능
-  protected position: string; //외부 접근 불가. 파생클래스에서는 접근 가능
-
-  //생성자 매개변수에 타입 지정
-  constructor(name: string, age: number, position: string) {
-    this.name = name;
-    this.age = age;
-    this.position = position;
-  }
-
-  add1ToAge(): number {
-    return this.age + 1;
-  }
+interface ICharacter {
+  name: string;
+  age: number;
+  //   moveSpeed: number; -> 이 필드는 private으로 하고싶은데 인터페이스에서 구현하면 무조건 public임
+  move(): void;
 }
 
-const person = new Person('sia', 20, 'developer'); //
-
-class Developer extends Person {
-  favoriteSkill: string;
-
+class Character implements ICharacter {
   constructor(
-    name: string,
-    age: number,
-    position: string,
-    favoriteSkill: string
-  ) {
-    super(name, age, position);
-    this.position = position;
-    this.favoriteSkill = favoriteSkill;
+    public name: string,
+    public age: number,
+    private moveSpeed: number
+  ) {} //생성자의 구현부를 생략하고 싶으면 'public' 접근제어자를 생성자의 매개변수로 달아주면됨. 인터페이스로 정의한 필드들은 무조건 public이기 때문에, private필드가 필요하면  인터페이스에서 구현하지 말고 따로 전달해야됨
+
+  move(): void {
+    console.log(`${this.moveSpeed} 속도로 이동한다`);
   }
 }
